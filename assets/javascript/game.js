948b11e3a9ccfc54fee2e49f393a22d5ad6f028c
Victoria Paul <victoriapaul@CodeBook-Pro.local>
2018-06-01 20:48:13 -0400

    tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Variables
 var computerChoices = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k", "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"];
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
 
 var wins = 0;
 var losses = 0;
@@ -21,7 +21,8 @@ function resetGuesses() {
 document.onkeyup = function(event) {
 
     var letter = event.key.toLowerCase();
-
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
     guesses.push(letter);
     guessesLeft --;
 
